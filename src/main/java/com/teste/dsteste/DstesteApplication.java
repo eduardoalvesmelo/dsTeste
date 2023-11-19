ec385f2107eac968cb3c5c6411704c29bbdefeee
Eduardo Alves <eduardoalvesmelo.2018@gmail.com>
2023-11-19 16:52:59 -0400

    Created projetct

---
+++ src/main/java/com/teste/dsteste/DstesteApplication.java
@@ -0,0 +1,13 @@
+package com.teste.dsteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstesteApplication.class, args);
+	}
+
+}
